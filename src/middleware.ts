3359cf85caba9db8eb92f58995a8f11c3d5bc3c8
Alexander <aleksander.misterkiewicz@outlook.com>
2024-10-08 20:07:07 +0200

    add middleware - admin route secure

---
+++ src/middleware.ts
@@ -0,0 +1,21 @@
+import { auth } from "./app/api/auth/[...nextauth]/route";
+import { NextResponse } from "next/server";
+
+export default auth((req) => {
+  const isLoggedIn = !!req.auth;
+  const isOnAdminPage = req.nextUrl.pathname.startsWith("/admin");
+  const isAdmin = req.auth?.user?.role === "ADMIN";
+
+  if (isOnAdminPage) {
+    if (!isLoggedIn) {
+      return NextResponse.redirect(new URL("/auth/signin", req.url));
+    }
+    if (!isAdmin) {
+      return NextResponse.redirect(new URL("/", req.url));
+    }
+  }
+});
+
+export const config = {
+  matcher: ["/admin/:path*", "/auth/signin"],
+};
