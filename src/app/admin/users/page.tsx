caeb602854b831f3620944b725b8f144b5d40a67
Alexander <aleksander.misterkiewicz@outlook.com>
2024-10-11 17:41:35 +0200

    add user list page

--- src/app/admin/users/page.tsx
+++ src/app/admin/users/page.tsx
@@ -1,14 +1,13 @@
-import { fetchHelperServer } from "@/lib/fetch-helper-server";
 import { UserList } from "@/components/admin/user-list";
-import { User } from "@/types/db";
+import { fetchHelperServer } from "@/lib/fetch-helper-server";
 
-export default async function UsersAdminPage() {
-  const users = (await fetchHelperServer("/api/admin/users")) as User[];
+export default async function UsersPage() {
+  const { users, pagination } = await fetchHelperServer("/api/admin/users");
 
   return (
     <div className="container mx-auto py-10">
       <h1 className="text-2xl font-bold mb-5">Manage Users</h1>
-      <UserList initialUsers={users} />
+      <UserList initialUsers={users} initialPagination={pagination} />
     </div>
   );
 }
