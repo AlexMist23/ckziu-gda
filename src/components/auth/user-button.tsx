d3d6497f8d3214443540ad7f229b2f7882c98539
Alexander <aleksander.misterkiewicz@outlook.com>
2024-10-11 17:45:01 +0200

    upd design of user button

--- src/components/auth/user-button.tsx
+++ src/components/auth/user-button.tsx
@@ -7,8 +7,11 @@ import {
   DropdownMenuItem,
   DropdownMenuLabel,
   DropdownMenuTrigger,
+  DropdownMenuSeparator,
 } from "@/components/ui/dropdown-menu";
 import { SignIn, SignOut } from "@/components/auth/auth-component";
+import Link from "next/link";
+import { User, Settings, LogOut } from "lucide-react";
 
 export default async function UserButton() {
   const session = await auth();
@@ -16,21 +19,23 @@ export default async function UserButton() {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="ghost" className="relative w-8 h-8 rounded-full">
-          <Avatar className="w-8 h-8">
+        <Button variant="ghost" className="relative h-10 w-10 rounded-full">
+          <Avatar className="h-10 w-10">
             {session.user.image && (
               <AvatarImage
                 src={session.user.image}
                 alt={session.user.name ?? ""}
               />
             )}
-            <AvatarFallback>{session.user.email}</AvatarFallback>
+            <AvatarFallback>
+              {session.user.email?.[0].toUpperCase()}
+            </AvatarFallback>
           </Avatar>
         </Button>
       </DropdownMenuTrigger>
-      <DropdownMenuContent className="w-56" align="end" forceMount>
+      <DropdownMenuContent className="w-64" align="end" forceMount>
         <DropdownMenuLabel className="font-normal">
-          <div className="flex flex-col space-y-1">
+          <div className="flex flex-col space-y-2">
             <p className="text-sm font-medium leading-none">
               {session.user.name}
             </p>
@@ -39,8 +44,31 @@ export default async function UserButton() {
             </p>
           </div>
         </DropdownMenuLabel>
-        <DropdownMenuItem>
-          <SignOut />
+        <DropdownMenuSeparator />
+        <DropdownMenuItem asChild>
+          <Link
+            href="/profile"
+            className="cursor-pointer flex items-center p-2"
+          >
+            <User className="mr-3 h-4 w-4" />
+            <span>Profile</span>
+          </Link>
+        </DropdownMenuItem>
+        <DropdownMenuItem asChild>
+          <Link
+            href="/profile/settings"
+            className="cursor-pointer flex items-center p-2"
+          >
+            <Settings className="mr-3 h-4 w-4" />
+            <span>Settings</span>
+          </Link>
+        </DropdownMenuItem>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem className="p-2">
+          <SignOut className="w-full flex items-center">
+            <LogOut className="mr-3 h-4 w-4" />
+            <span>Sign out</span>
+          </SignOut>
         </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
