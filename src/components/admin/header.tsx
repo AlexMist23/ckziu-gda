ccbe9b081fafbbc8e3540a0774a2eed0aef4213a
Alexander <aleksander.misterkiewicz@outlook.com>
2024-10-11 17:44:25 +0200

    add admin header

---
+++ src/components/admin/header.tsx
@@ -0,0 +1,14 @@
+import { CustomBreadcrumbs } from "./breadcrumbs";
+
+export const AdminHeader = () => {
+  return (
+    <header className="bg-card shadow-md py-4 px-6">
+      <div className="flex justify-between items-center mb-4">
+        <h2 className="text-xl font-semibold text-foreground">
+          Admin Dashboard
+        </h2>
+      </div>
+      <CustomBreadcrumbs />
+    </header>
+  );
+};
