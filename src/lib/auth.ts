98403b962e1776fe694314b950b365ae76ec3000
Alexander <aleksander.misterkiewicz@outlook.com>
2024-10-12 22:56:57 +0200

    authConfig update

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,31 +1,31 @@
-import NextAuth, { NextAuthConfig } from "next-auth";
-// import PostgresAdapter from "@auth/pg-adapter";
+import NextAuth from "next-auth";
+import PostgresAdapter from "@auth/pg-adapter";
 import GitHub from "next-auth/providers/github";
 import Google from "next-auth/providers/google";
+import { Pool } from "@neondatabase/serverless";
 
-// import { Pool } from "@neondatabase/serverless";
-export const authConfig: NextAuthConfig = {
-  // adapter: PostgresAdapter(
-  //   new Pool({
-  //     connectionString: process.env.POSTGRES_URL,
-  //   }) as unknown as import("pg").Pool
-  // ),
-  providers: [GitHub, Google],
-  callbacks: {
-    session({ session, user }) {
-      if (session.user) {
-        session.user.id = user.id;
-        session.user.role = user.role;
-      }
-      return session;
+export const { handlers, auth, signIn, signOut } = NextAuth(() => {
+  const pool = new Pool({
+    connectionString: process.env.POSTGRES_URL,
+  }) as unknown as import("pg").Pool;
+
+  return {
+    adapter: PostgresAdapter(pool),
+    providers: [GitHub, Google],
+    callbacks: {
+      session({ session, user }) {
+        if (session.user) {
+          session.user.id = user.id;
+          session.user.role = user.role;
+        }
+        return session;
+      },
     },
-  },
-  pages: {
-    signIn: "/auth/signin",
-    error: "/auth/error",
-    signOut: "/auth/signout",
-  },
-  trustHost: true,
-};
-
-export const { handlers, auth, signIn, signOut } = NextAuth(authConfig);
+    pages: {
+      signIn: "/auth/signin",
+      error: "/auth/error",
+      signOut: "/auth/signout",
+    },
+    trustHost: true,
+  };
+});
