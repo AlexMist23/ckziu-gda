f8822800c25671ab200ba0267a9b96c06961a24d
Alexander <aleksander.misterkiewicz@outlook.com>
2024-10-21 12:17:49 +0200

    del everything

--- src/types/app.types.ts
+++
@@ -1,7 +0,0 @@
-import { Teacher } from "./db.types";
-
-export type TeacherWithSubjects = Teacher & {
-  subject_ids: number[];
-};
-
-export type TeacherFormData = Omit<TeacherWithSubjects, "id">;
