5e9f2779b9a3f30ae1f177c58a4a2e7255663dd7
Alexander <aleksander.misterkiewicz@outlook.com>
2024-10-22 21:31:18 +0200

    middleware update

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,25 +1,24 @@
 export { auth as middleware } from "@/lib/auth";
-// import { NextResponse } from "next/server";
-// import { auth } from "./lib/auth";
-//
-// export default auth((req) => {
-//   const isLoggedIn = !!req.auth;
-//   const isAdminRoute =
-//     req.nextUrl.pathname.startsWith("/admin") ||
-//     req.nextUrl.pathname.startsWith("/api/admin");
-//   const isAdmin = req.auth?.user?.role === "admin";
-//
-//   if (isAdminRoute) {
-//     if (!isLoggedIn) {
-//       return NextResponse.redirect(new URL("/api/auth/signin", req.url));
-//     }
-//     if (!isAdmin) {
-//       return NextResponse.redirect(new URL("/", req.url));
-//     }
-//   }
-// });
-//
-// export const config = {
-//   matcher: ["/admin/:path*", "/api/admin/:path*"],
-// };
-//
+import { NextResponse } from "next/server";
+import { auth } from "./lib/auth";
+
+export default auth((req) => {
+  const isLoggedIn = !!req.auth;
+  const isAdminRoute =
+    req.nextUrl.pathname.startsWith("/admin") ||
+    req.nextUrl.pathname.startsWith("/api/admin");
+  const isAdmin = req.auth?.user?.role === "admin";
+
+  if (isAdminRoute) {
+    if (!isLoggedIn) {
+      return NextResponse.redirect(new URL("/api/auth/signin", req.url));
+    }
+    if (!isAdmin) {
+      return NextResponse.redirect(new URL("/", req.url));
+    }
+  }
+});
+
+export const config = {
+  matcher: ["/admin/:path*", "/api/admin/:path*"],
+};
